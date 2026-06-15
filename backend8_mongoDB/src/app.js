172c5b68782cf07c0432665d893f0844b1dc7889
Khushi-1803 <khushi011504@gmail.com>
2026-06-15 14:25:29 +0530

    done for 15-06-26

--- backend8_mongoDB/src/app.js
+++ backend8_mongoDB/src/app.js
@@ -1,4 +1,49 @@
 const express = require("express");
-const app = express();
-module.exports=app
+const noteModel = require("./models/notes.model.js");
 
+const app = express();
+
+app.use(express.json());
+
+app.post("/notes", async (req, res) => {
+    const data = req.body;
+
+    await noteModel.create({
+        title: data.title,
+        description: data.description
+    });
+
+    res.status(201).json({
+        message: "Note created successfully"
+    });
+});
+
+app.get("/notes",async(req,res)=>{
+    const notes = await noteModel.find();
+
+    res.status(200).json({
+        message:"notes fetched sucessfully",
+        notes:notes
+    })
+})
+
+app.delete("/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    await noteModel.findByIdAndDelete(id);
+
+    res.status(201).json({
+        message:"Note deleted sucessfully"
+    })
+})
+
+app.patch("/notes/:id",async(req,res)=>{
+    const id = req.params.id
+    const description = req.body.description
+    await noteModel.findOneAndUpdate({_id:id},{description:description})
+
+    res.status(201).json({
+        message:"Note updated sucessfully"
+    })
+})
+
+module.exports = app;
