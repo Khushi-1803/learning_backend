55df660899979366967f93e6d4063837997e7042
Khushi-1803 <khushi011504@gmail.com>
2026-06-16 21:10:26 +0530

    completed!

--- backend6-form_controller/backend/server.js
+++ backend6-form_controller/backend/server.js
@@ -1,14 +1,47 @@
 const express = require("express");
 const connectDB = require("./db/db.js");
+const userModel = require("./models/user.models.js");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
 
-app.use(express.json());
+app.use(cors({
+    origin: "http://127.0.0.1:5500"
+}));
+
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
-connectDB()
 
-app.get("/", (req, res) => {
-    res.send("Server is running!");
+connectDB();
+
+// Register Route
+app.post("/register", async (req, res) => {
+    try {
+        const data = req.body;
+
+        await userModel.create({
+            name: data.name,
+            email: data.email,
+            password: data.password
+        });
+
+        console.log(req.body);
+
+        res.redirect("/home");
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Something went wrong");
+    }
+});
+
+// Home Route
+app.get("/home", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../frontend/Home.html")
+    );
 });
 
 app.listen(PORT, () => {
